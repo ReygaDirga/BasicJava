2cda87a1b82b7db6ffb60d3382af84241c2e95df
ReygaDirga <reygadirg@gmail.com>
2025-02-19 21:33:51 +0700

    update java Vscode

---
+++ Java VsCode/Latihan1/src/Book/Library.java
@@ -0,0 +1,40 @@
+package Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> books;
+
+        public Library() {
+            this.books = new ArrayList<>();
+        }
+
+        public void addBook(Book book) {
+            books.add(book);
+            System.out.println("Book added to library: " + book.getTitle());
+        }
+
+        public void borrowBook(Borrower borrower, Book book) {
+            if (books.contains(book)) {
+                books.remove(book);
+                borrower.borrowBook(book);
+            }
+        }
+
+        public void returnBook(Borrower borrower, Book book) {
+            books.add(book);
+            borrower.returnBook(book);
+        }
+
+        public void displayLibraryBooks() {
+            System.out.println("Books in Library:");
+            for (Book book : books) {
+                System.out.println("Title: " + book.getTitle());
+                System.out.println("ISBN: " + book.getIsbn());
+                System.out.println("Author: " + book.getAuthor().getName());
+                System.out.println("Biography: " + book.getAuthor().getBiography());
+                System.out.println("----------------------");
+            }
+        }
+}
